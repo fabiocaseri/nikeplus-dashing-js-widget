23c389430cfa30cb69fee39d3e4b4fdf45eee20d
Fabio Caseri <fabio.caseri@gmail.com>
2013-04-26 21:41:12 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Nike+ Dashing widget.
+
+[Nike+](http://nikeplus.nike.com/plus/) widget for [Dashing](http://fabiocaseri.github.io/dashing-js), ported from [@danillotuhumury's work](https://github.com/danillotuhumury/nikeplus-dashing-widget). It displays your last activity data (NikeFuel points, calories, distance and duration). All of the previously mentioned options are shown in a slideshow. Examples of the widget can be viewed [below](https://github.com/fabiocaseri/nikeplus-dashing-js-widget#preview).
+Ported from [@danillotuhumury's work](https://github.com/danillotuhumury/nikeplus-dashing-widget)
+
+## Installation
+
+```shell
+$ dashing-js install GIST_ID
+```
+
+## Usage
+
+To actually use the widget on your own Dashboard, you'll have to [request](https://developer.nike.com) an Access Token from Nike+ (Nike didn't release the OAuth API yet.). With this Access Token you'll be able to retrieve data from you Nike+ account.
+
+To include the widget on your dashboard, add the following snippet to the dashboard layout file:
+
+```html
+<li data-row="1" data-col="1" data-sizex="1" data-sizey="1">
+  <div data-id="nikeplus" data-view="Nikeplus"></div>
+</li>
+```
+
+On to the next part, rename the file `jobs/nikeplus_access_token.js.sample` in `jobs/nikeplus_access_token.js` and enter the Access Token:
+
+```javascript
+// You Nike+ access token
+var access_token = 'YOUR_ACCESS_TOKEN_HERE';
+```
+
+## Preview
+![fuel](https://f.cloud.github.com/assets/1248374/370342/396f97f8-a2ef-11e2-815f-d9246828212d.png) 
+![distance](https://f.cloud.github.com/assets/1248374/370344/397bf476-a2ef-11e2-8fb5-3795f642ff05.png)
---
+++ nikeplus.js
@@ -0,0 +1,65 @@
+var http = require('https')
+  , access_token = require('./nikeplus_access_token');
+
+var options = {
+  hostname: 'api.nike.com',
+  path: '/me/sport/activities?access_token=' + access_token,
+  headers: {
+    'appid': 'nikeplus-dashing-js-widget',
+    'Accept': 'application/json'
+  }
+};
+function fetchNikePlus() {
+  http.get(options, function(res) {
+    if (res.statusCode == 200) {
+      var body = '';
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
+        console.log(body);
+        body = JSON.parse(body);
+        var activities = body.data;
+        if (activities && activities.length) {
+          var summary = activities[0].metricSummary;
+          summary && send_event('nikeplus', {
+            calories: summary.calories,
+            fuel: summary.fuel,
+            distance: parseFloat(summary.distance, 10).toFixed(2),
+            duration: summary.duration
+          });
+        }
+      });
+    }
+  }).on('error', function(err) {
+    console.log('Error reading from nikeplus: ', err);
+  });
+}
+
+setInterval(fetchNikePlus, 15 * 60 * 1000);
+fetchNikePlus();
+
+/*
+require "net/http"
+require "json"
+require "nike_v2"
+
+# widget configuration
+access_token = "ACCESS_TOKEN"
+
+SCHEDULER.every "15m", :first_in => 0 do |job|
+  person        = NikeV2::Person.new(access_token: access_token)
+  activity      = person.activities.first
+  
+  if activity
+    summary = activity.fetch_data['metricSummary']
+
+    send_event "nikeplus", { 
+      calories: summary['calories'],
+      fuel:     summary['fuel'],
+      distance: summary['distance'].round(2),
+      duration: summary['duration'][0...-4]
+    }
+  end
+end
+*/
